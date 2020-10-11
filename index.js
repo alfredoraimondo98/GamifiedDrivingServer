64d41d7a85a502b42edc59376fcf04b5c5de27ef
unknown <alfredoraimondo98@gmail.com>
2020-10-11 18:24:06 +0200

    Cors headers

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use((req,res,next) => {
   res.json({ message: 'This route is CORS-enabled for an allowed origin.' });
 }) 
  */
-app.use(cors());
+//app.use(cors());
 
 const passport = require("passport")
 
