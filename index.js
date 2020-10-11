1ee2aab61db1adeab979d7511cf483446524df29
unknown <alfredoraimondo98@gmail.com>
2020-10-11 18:14:39 +0200

    cors

--- index.js
+++ index.js
@@ -8,31 +8,12 @@ app.use(bodyParser.json());
 
 const cors = require('cors');
 
-const allowedOrigins = [
-    'capacitor://localhost',
-    'ionic://localhost',
-    'http://localhost',
-    'http://localhost:8100'
-  ];
 
-// Reflect the origin if it's in the allowed list or not defined (cURL, Postman, etc.)
-const corsOptions = {
-    origin: (origin, callback) => {
-      if (allowedOrigins.includes(origin) || !origin) {
-        callback(null, true);
-      } else {
-        callback(new Error('Origin not allowed by CORS'));
-      }
-    }
-  }
-  
- // Enable preflight requests for all routes
-app.options('*', cors(corsOptions));
 
-app.get('/cors', cors(corsOptions), (req, res, next) => {
+/* app.get('/auth', cors(corsOptions), (req, res, next) => {
   res.json({ message: 'This route is CORS-enabled for an allowed origin.' });
 }) 
-
+ */
 app.use(cors());
 
 const passport = require("passport")
--- routes/auth.js
+++ routes/auth.js
@@ -7,6 +7,31 @@ const FB = require('fb');
 const authController = require('../controllers/auth');
 const isAuth = require('../middleware/is-auth');
 
+
+
+const cors = require('cors');
+const allowedOrigins = [
+    'capacitor://localhost',
+    'ionic://localhost',
+    'http://localhost',
+    'http://localhost:8100'
+  ];
+
+// Reflect the origin if it's in the allowed list or not defined (cURL, Postman, etc.)
+const corsOptions = {
+    origin: (origin, callback) => {
+      if (allowedOrigins.includes(origin) || !origin) {
+        callback(null, true);
+      } else {
+        callback(new Error('Origin not allowed by CORS'));
+      }
+    }
+  }
+  
+ // Enable preflight requests for all routes
+router.options('*', cors(corsOptions));
+
+
 router.post('/register', [
     //body('password').trim().isLength({ min : 5}).withMessage('Inserire una password valida (almeno 8 caratteri')
 
@@ -17,10 +42,12 @@ router.post('/login', authController.loginApp);
 router.get('/login/me', isAuth, authController.loginMe);
 router.post('/checkEmail', authController.checkEmail);
 
-router.get('/facebook',  passport.authenticate("facebook"));
+router.get('/facebook', cors(corsOptions),  passport.authenticate("facebook"));
 router.get('/facebook/callback', passport.authenticate('facebook', { successRedirect: '/auth/successLoginFacebook', failureRedirect: '/auth/errorLogin' }));
 router.get('/successLoginFacebook', authController.successFb);
 router.post('/errorLogin', authController.errorFb);
+
+
 
 
 module.exports = router;
