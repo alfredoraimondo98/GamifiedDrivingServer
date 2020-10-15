1cebfaac3eed7d098509f29a7cb083aaf8aff4e2
unknown <alfredoraimondo98@gmail.com>
2020-10-15 20:58:22 +0200

    Sessione getPosizione

--- controllers/sessione.js
+++ controllers/sessione.js
@@ -10,17 +10,46 @@ const queries = require('../utils/queries')
 exports.getPosizione = (req,res,next) => {
     var request = require("request")
 
-    var lat = req.body.lat;
-    var lon = req.body.lon;
+    var lat = req.body.lat; //latitudine
+    var lon = req.body.lon; //longitudine
+    var around = '50.0' //precisione di calcolo della posizione (es: 50 metri vicino alle coordinate)
+   
+    
+    console.log(lat, lon);
 
-
-    var url = `http://overpass-api.de/api/interpreter?data=[out:json][timeout:25];%20(%20way(around:5.0,${lat},${lon});%20);%20out%20body;%20%3E;%20out%20skel%20qt;`
+    var url = `http://overpass-api.de/api/interpreter?data=[out:json][timeout:25];%20(%20way(around:${around},${lat},${lon});%20);%20out%20body;%20%3E;%20out%20skel%20qt;`
 
     request({ url: url, json: true}, (error, response, body) => {
         if (!error && response.statusCode === 200) {
             console.log(body.elements[0].tags) // Print the json response
+
+            let highway = body.elements[0].tags.highway; //Tipo strada
+            let maxspeed = body.elements[0].tags.maxspeed;  //velocità max sulla strada
+            let name = body.elements[0].tags.name;  //nome strada
+            console.log(body);
+
+            //Verifica la disponibilità dei dati, altrimenti setta i valori di default
+            if(!body.elements[0].tags.highway){
+                highway = 'Unclassified'
+            }
+            if(!body.elements[0].tags.maxspeed){
+                maxspeed = 50
+            }
+            if(!body.elements[0].tags.name){
+                if(body.elements[0].tags.ref){
+                    name = body.elements[0].tags.ref;
+                }
+                else{
+                    name = highway
+                }
+            }
+
             res.status(201).json({
-               dati:  body.elements[0].tags
+               hyghway : highway,
+               maxspeed : maxspeed,
+               name : name,
+             // all: body,
+             // dati:  body.elements[0].tags
             })
         }
         else{
@@ -29,5 +58,5 @@ exports.getPosizione = (req,res,next) => {
             })
         }
     })
-    
+
 }
